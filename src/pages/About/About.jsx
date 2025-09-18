8c02aff16870f500107d77ce5889ec8895a82d4b
mohammd-clawd <mm32395982020@gmail.com>
2025-09-19 00:14:00 +0300

    About

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,7 +2,48 @@ import "./About.css"
 
 function About() {
   return (
-    <div>About</div>
+    <div className="about-container">
+      <div className="img-heder"></div>
+       <div className="poop-heder">
+         <span>
+          In nec purus eget neque accumsan finibus. Duis condimentum elit ut libero commodo iaculis. Donec augue diam, tristique et ultricies nec, consectetur quis enim. Nullam id
+          rutrum ex. Aliquam a lectus id lacus rhoncus dapibus non ac justo. Vivamus lacinia vestibulum metus in dapibus. Vestibulum sit amet sollicitudin enim. Ut id interdum turpis.
+          Curabitur porta auctor quam, pretium facilisis nisl. Pellentesque efficitur elit ante, vel vulputate tortor blandit nec.
+         </span>
+         <h2 className="span-col">Quisque erat urna, congue et libero in <br/>
+            eleifend euismod velit.
+          </h2>
+         <h6 className="span-col">
+          In nec purus eget neque accumsan finibus. Duis condimentum elit ut libero commodo iaculis. Donec augue diam, tristique et ultricies nec, consectetur quis enim. Nullam id
+          rutrum ex. Aliquam a lectus id lacus rhoncus dapibus non ac justo. Vivamus lacinia vestibulum metus in dapibus. Vestibulum sit amet sollicitudin enim. Ut id interdum turpis.
+          Curabitur porta auctor quam, pretium facilisis nisl. Pellentesque efficitur elit ante, vel vulputate tortor blandit nec.
+         </h6>
+         <div className="span-img"></div>
+         <h3>Duis convallis luctus pretium <br />
+             Pellentesque habitant morbi.
+         </h3>
+                <p className="cspan-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse
+                  ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. <br/> <br />
+
+                  In fermentum mi ut sapien semper, a sagittis lorem vulputate. Integer gravida,
+                  dui eget aliquet tempus, turpis orci vehicula ipsum, eget porttitor sapien tortor
+                  at neque. Cras id pulvinar lacus, ac volutpat neque. Ut at magna id justo
+                  bibendum lobortis. Integer tortor nulla, ultricies et nisi sit amet, interdum
+                  dictum felis. In semper laoreet dui vitae pharetra. Etiam sit amet molestie nulla,
+                  eu efficitur orci. Praesent rutrum ante justo, eget malesuada ante ornare ac. Ut
+                  dignissim blandit urna, eget euismod leo rhoncus nec. Vestibulum ante ipsum
+                  primis in faucibus orci luctus et ultrices posuere cubilia curae; Quisque lobortis
+                  libero ante. Nullam in feugiat erat. Aenean sed justo dapibus, sodales nisi ut,
+                  fringilla lorem. Vestibulum in orci ac nisl condimentum fermentum at et sem.
+                  Curabitur fermentum dolor eu lacus consectetur varius.
+                </p>
+                <p className="foter-span">In nec purus eget neque accumsan finibus. Duis condimentum elit ut libero commodo iaculis. Donec augue diam, tristique et ultricies nec, consectetur quis enim. Nullam id
+                      rutrum ex. Aliquam a lectus id lacus rhoncus dapibus non ac justo. Vivamus lacinia vestibulum metus in dapibus. Vestibulum sit amet sollicitudin enim. Ut id interdum turpis.
+                      Curabitur porta auctor quam, pretium facilisis nisl. Pellentesque efficitur elit ante, vel vulputate tortor blandit nec.
+                </p>
+       </div>
+    </div>
   )
 }
 
