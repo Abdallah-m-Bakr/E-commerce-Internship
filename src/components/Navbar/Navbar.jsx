0d64e0627a3ec90d6c5630bc435a8f46dbf5c75b
mohamedkhalil04 <mohamedsalah2932019@gmail.com>
2025-09-18 18:10:18 +0300

    header&blog

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,13 @@ import "./Navbar.css"
 import { NavLink } from 'react-router-dom';
 import logo from '../../assets/images/logo.png'
 function Navbar() {
+
+  const activeStyle = ({ isActive }) => {
+    return {
+      color: isActive ? "#49BFAA" : "",
+      backgroundColor: isActive ? "#F0FAFF" : ""
+    }
+  };
   return (
     <>
       <div>
@@ -10,78 +17,68 @@ function Navbar() {
           <p className="duetotext text-light">Due to current circumstances, there may be slight delays in order processing</p>
         </div>
         {/* 1st navbar */}
-        <div className="container d-flex">
+        <div className="container d-flex pt-2 first-navbar">
           {/* navs */}
           <div>
-            <ul className='d-flex '>
-              <li><NavLink >About Us</NavLink><div className='linenav'></div></li>
-              <li><NavLink >Compare</NavLink><div className='linenav'></div></li>
-              <li><NavLink >Wishlist</NavLink><div className='linenav'></div></li>
+            <ul className='d-flex justify-content-evenly navul-1'>
+              <li><NavLink to="/about" >About Us</NavLink></li>
+              <li><NavLink >Compare</NavLink></li>
+              <li><NavLink >Wishlist</NavLink></li>
             </ul>
           </div>
           {/* text */}
-          <div className="secure-delivery d-flex text-muted">
-            <i class="fa-solid fa-user-shield pt-1"></i>
-            <p>100% secure delivery without contacting the courier</p>
-          </div>
-          <div className="d-flex">
-            <div className="line-vertical "></div>
-            <p className="text-muted">Need help? call us:<b className="number">+00200 500</b></p>
-          </div>
-          {/* dropdown */}
-          <div className="selection">
-            <select className="select">
-              <option value="English">English</option>
-              <option value="Arabic">Arabic</option>
-            </select>
-            <select className="select">
-              <option >USD</option>
-              <option >EUR</option>
-              <option >EGP</option>
-              <option >GBP</option>
-              <option >AED</option>
-              <option >KWD</option>
-              <option >SAR</option>
-            </select>
+          <div className="d-flex justify-content-end text-for-navbar">
+            <div className="secure-delivery d-flex text-muted">
+              <i class="fa-solid fa-user-shield pt-1"></i>
+              <p>100% secure delivery without contacting the courier</p>
+            </div>
+            <div className="d-flex">
+              <div className="line-vertical pt-3 "></div>
+              <p className="text-muted">Need help? call us:<b className="number">+00200 500</b></p>
+              <div className="line-vertical pt-3 "></div>
+            </div>
+            {/* dropdown */}
+            <div className="selection">
+              <select className="select">
+                <option value="English">English</option>
+                <option value="Arabic">Arabic</option>
+              </select>
+            </div>
           </div>
         </div>
+        {/* line */}
+        <hr className="horizontail-line pt-1"/>
 
         <div className='header d-flex justify-content-between container '>
           <img className='img' src={logo} alt='logo' width={200} height={80} />
           {/* search */}
-          <div className="input-text border rounded" >
+          <div className="input-text border-0 rounded d-flex justify-content-between" >
             <input className='search border-0' type="text" placeholder="Search for products, fruit, meet, eggs, etc..." />
             <i class="fa-solid fa-magnifying-glass"></i>
           </div>
           {/* user $ price & cart*/}
           <div className="user-price-cart d-flex justify-content-evenly">
             <div className="user"><i class="fa-solid fa-user"></i></div>
-            <div className="price">$0.00</div>
+            <div className="price"><p>$0.00</p></div>
             <div className="cart">
               <i class="fa-solid fa-bucket"></i>
               <span className="number-cart">0</span></div>
           </div>
         </div>
+        
         {/* 2nd navbar */}
-        <div className="d-flex justify-content-between navbar2">
-         <div className="TotalProducts">
-          <div className="d-flex justify-content-evenly allcategories">
-          <i class="fa-solid fa-bars "></i>
-          <p>ALL CATEGORIES</p>
-          <i class="fa-solid fa-chevron-down "></i>
-          </div>
-          <p className="total50 text-muted">TOTAL 50 PRODUCTS</p>
-         </div>
-          
+        <div className="navbar2 container">
           <ul className='d-flex justify-content-evenly navul'>
-            <li><NavLink >HOME</NavLink></li>
-            <li><NavLink >SHOP</NavLink></li>
-            <li><NavLink >BLOG</NavLink></li>
-            <li><NavLink >CONTACT</NavLink></li>
+            <li><NavLink style={activeStyle} to="/" >HOME</NavLink></li>
+            <li><NavLink style={activeStyle} to="/shop" >SHOP</NavLink></li>
+            <li><NavLink style={activeStyle} to="/blog" >BLOG</NavLink></li>
+            <li><NavLink style={activeStyle} to="/contact" >CONTACT</NavLink></li>
+            <li><NavLink style={activeStyle} to="/login" >LOG IN</NavLink></li>
+            <li><NavLink style={activeStyle} to="/register" >REGISTER</NavLink></li>
           </ul>
-
         </div>
       </div>
+      <hr className="horizontail-line pt-1"/>
     </>
 
   )
--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,8 +1,58 @@
 import "./Blog.css"
-
+import bottle from '../../assets/images/bottle.png'
+import facebook from '../../assets/images/facebook_img.png'
+import instagram from '../../assets/images/istagram_img.png'
+import twitter from '../../assets/images/x_img.png'
+import reddit from '../../assets/images/reddit_img.png'
+import pinterest from '../../assets/images/printerest_img.png'
+import starbucks from '../../assets/images/starbucks.png'
+import coffie from '../../assets/images/coffie.png'
 function Blog() {
   return (
-    <div>Blog</div>
+    <div className="blog-page d-flex justify-content-evenly">
+      <div className="left">
+        <div className="water-bottle">
+          <img src={bottle} alt="water" />
+        </div>
+        <div className="grocery1">
+          <p>Grocery</p>
+        </div>
+        <div className="coffie-nut">
+          <img src={coffie} alt="coffie" />
+        </div>
+        <div className="grocery2">
+          <p>Grocery</p>
+        </div>
+      </div>
+      <div className="right">
+        <div className="recent-posts">
+          <h2>Recent Posts</h2>
+        </div>
+        <div className="social-media">
+          <h2>social media</h2>
+          <img src={facebook} alt="facebook" className="rounded" />
+          <img src={instagram} alt="instagram"  className="rounded"/>
+          <img src={twitter} alt="x"  className="rounded"/>
+          <img src={reddit} alt="reddit" className="rounded" />
+          <img src={pinterest} alt="pinterest"  className="rounded"/>
+        </div>
+        <div className="banner">
+          <h2>WIDGET BANNER</h2>
+          <img src={starbucks} alt="starbucks" />
+        </div>
+        <div className="tags">
+        <h2>Tags</h2>
+        <span>ecommerce</span>
+        <span>food</span>
+        <span>grocery</span>
+        <span>kiptheme</span>
+        <span>organic</span>
+        <span>shop</span>
+        <span>shopify</span>
+        <span>store</span>
+      </div>
+      </div>
+    </div >
   )
 }
 
