ca3611ad5e3d8d17d857c7161ec5b9ffca4c985a
Abdallah <Abdallah.m.abobakr@gmail.com>
2025-09-18 20:26:23 +0300

    login register desing

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import store from './redux/stor.js'
 import { Provider } from 'react-redux'
-// import '@fortawesome/fontawesome-free/css/all.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 
 
