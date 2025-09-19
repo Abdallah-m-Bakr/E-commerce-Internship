4eba3ff95bb4b39ec122d2dfecd098340da684f0
mohamedkhalil04 <mohamedsalah2932019@gmail.com>
2025-09-19 20:27:14 +0300

    blog page

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -14,22 +14,38 @@ function Blog() {
         <div className="water-bottle">
           <img src={bottle} alt="water" />
         </div>
-        <div className="grocery1">
-          <p>Grocery</p>
+        <div className="grocery1 grocery">
+          <p className="text-muted">Grocery</p>
+          <h2>But i must explain to you how all those mistaken idea</h2>
+          <span className="text-muted">jan 13 2025 &nbsp; <span className="text-dark">Sinan ISIK</span> </span>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat vero, quis sed ullam nihil deserunt, esse nam quasi repudiandae facilis debitis perspiciatis.</p>
         </div>
         <div className="coffie-nut">
           <img src={coffie} alt="coffie" />
         </div>
-        <div className="grocery2">
-          <p>Grocery</p>
+        <div className="grocery2 grocery">
+          <p className="text-muted">Grocery</p>
+          <h2>the problem with typefaces on the web</h2>
+          <span className="text-muted">jan 13 2025 &nbsp; <span className="text-dark">Sinan ISIK</span> </span>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat vero, quis sed ullam nihil deserunt, esse nam quasi repudiandae facilis debitis perspiciatis.</p>
         </div>
       </div>
       <div className="right">
-        <div className="recent-posts">
-          <h2>Recent Posts</h2>
+        <div className="recent-posts mb-4">
+          <h2>RECENT POSTS</h2>
+          <div className="border p-3">
+            <div className="d-flex justify-content-between posts-content">
+              <img src={bottle} alt="water" width={40} height={40} />
+              <p>But i must explain to you how all those mistaken idea</p>
+            </div>
+            <div className="d-flex justify-content-between posts-content">
+              <img src={coffie} alt="coffie" width={40} height={40} />
+              <p>the problem with typefaces on the web</p>
+            </div>
+          </div>
         </div>
-        <div className="social-media">
-          <h2>social media</h2>
+        <div className="social-media mb-4">
+          <h2>SOCIAL MEDIA</h2>
           <img src={facebook} alt="facebook" className="rounded" />
           <img src={instagram} alt="instagram"  className="rounded"/>
           <img src={twitter} alt="x"  className="rounded"/>
