a5f8a43ede50b98b815aa3df858c6fac8edbcfe1
Abdallah <Abdallah.m.abobakr@gmail.com>
2025-09-24 15:30:57 +0300

    timer

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,27 +1,27 @@
-import "./hero.css"
-import {Link} from 'react-router-dom'
+import "./hero.css";
+import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import { useEffect  } from "react";
+import { useEffect } from "react";
 import { useProducts } from "../../context/ProductContext";
 import Loader from "../../components/Loader/Loader";
+import Timer from "../Timer/Timer";
 
-
-function Hero(){
-  const {t,i18n}=useTranslation();
+function Hero() {
+  const { t, i18n } = useTranslation();
   useEffect(() => {
-    // i18n.changeLanguage('ar'); 
-  },[i18n])
+    // i18n.changeLanguage('ar');
+  }, [i18n]);
 
   // context hero
   // const { homeProducts } = useContext(HomeContext);
 
-const { filteredProducts: products, loading, error } = useProducts();
+  const { filteredProducts: products, loading, error } = useProducts();
   if (loading) return <Loader />;
   if (error) return <div className="text-danger">{error}</div>;
   // Limit to first 4 products for Home
   const homeProducts = products.slice(0, 5);
 
-    return(
+  return (
     <div className="container">
       {/* hero section */}
       <div className="hero text-capitalize">
@@ -31,74 +31,116 @@ const { filteredProducts: products, loading, error } = useProducts();
               <p>{t("EXCLUSIVE OFFER")}</p>
               <span>{t("-20% OFF")}</span>
             </div>
-            <h2 className="fw-bold fs-1">{t("specialist in the grocery store")}</h2>
+            <h2 className="fw-bold fs-1">
+              {t("specialist in the grocery store")}
+            </h2>
             <p>{t("only this week.don't miss...")}</p>
-            <span>{t("from")}</span><span className="fw-bold fs-2 text-danger">$7.99</span><br/>
-            <Link to="/shop" className="btn px-3">{t("shop now")} <i className="fas fa-right-long"></i></Link>
+            <span>{t("from")}</span>
+            <span className="fw-bold fs-2 text-danger">$7.99</span>
+            <br />
+            <Link to="/shop" className="btn px-3">
+              {t("shop now")} <i className="fas fa-right-long"></i>
+            </Link>
           </div>
         </div>
         <div className="tag-line">
-          <span><b>{t("100% secure delivery")}</b> {t("without contacting the couier")}</span>
-          <Link to="/shop" className="btn px-3">{t("shop now")}</Link>
+          <span>
+            <b>{t("100% secure delivery")}</b>{" "}
+            {t("without contacting the couier")}
+          </span>
+          <Link to="/shop" className="btn px-3">
+            {t("shop now")}
+          </Link>
         </div>
       </div>
       {/* offer down section */}
       <div className="offer-down">
-        <div className="timer d-flex flex-md-row flex-column m-4">
-          <span>
-            <h4 className="text-primary">{t("special offers of the week!")}</h4>
-            <p>{t("Enjoy our special offers of the week! Get amazing discounts on the latest clothes and accessories. Don’t miss out, shop now!")}</p>
-          </span>
-          <div className="counter">
-            <div className="days bg-danger text-light">71</div><b>:</b>
-            <div className="hours bg-danger text-light">14</div><b>:</b>
-            <div className="min bg-danger text-light">43</div><b>:</b>
-            <div className="sec bg-danger text-light">16</div>
-          </div>
-        </div>
-        <div className="offer-cards border border-danger border-2 rounded row row-col-12">
-          {homeProducts.slice(0,5).map((p) => (
-          <div key={p.id} className="offer-card col-md-6 col-lg-4 col-xl border border-1 viewer-to-left">
-            <div className="top-card position-relative">
-              <img src={Array.isArray(p.images) ? p.images[0] : p.images} alt={p.title}/>
+        <div className="timer d-flex flex-column flex-md-row justify-content-center align-items-center text-center gap-4 my-4 flex-wrap">
+  <div className="text-section" style={{ maxWidth: "500px" }}>
+    <h4 className="text-primary">{t("special offers of the week!")}</h4>
+    <p>{t("Enjoy our special offers of the week! Get amazing discounts on the latest clothes and accessories. Don’t miss out, shop now!")}</p>
+  </div>
+  <Timer />
+</div>
 
-              <div className="offer position-absolute top-0 left-0 m-2">{p.discountPercentage}%</div>
+
+        <div className="offer-cards border border-danger border-2 rounded row row-col-12">
+          {homeProducts.slice(0, 5).map((p) => (
+            <div
+              key={p.id}
+              className="offer-card col-md-6 col-lg-4 col-xl border border-1 viewer-to-left"
+            >
+              <div className="top-card position-relative">
+                <img
+                  src={Array.isArray(p.images) ? p.images[0] : p.images}
+                  alt={p.title}
+                />
+
+                <div className="offer position-absolute top-0 left-0 m-2">
+                  {p.discountPercentage}%
+                </div>
+              </div>
+              <div className="body-card p-3">
+                <h5 className="title">{t(p.title)}</h5>
+                <div className="rate d-flex gap-2">
+                  <div className="text-warning">
+                    {p.rating}
+                    <i className="fas fa-star"></i>
+                  </div>
+                  <span>
+                    {p.reviews.length}
+                    {t(" review")}
+                  </span>
+                </div>
+                <div className="offer-price d-flex gap-2">
+                  <div className="before-offer text-decoration-line-through">
+                    ${(p.price / (1 - p.discountPercentage / 100)).toFixed(2)}
+                  </div>
+                  <div className="after-offer text-danger fs-5">${p.price}</div>
+                </div>
+                <div className="bar">
+                  <div style={{ width: p.stock + "%" }}></div>
+                </div>
+                <p className="pt-3 text-center">
+                  {t("the available products:")}{" "}
+                  <span className="fw-bold fs-3 text-primary">{p.stock}</span>
+                </p>
+              </div>
             </div>
-            <div className="body-card p-3">
-              <h5 className="title">{t(p.title)}</h5>
-              <div className="rate d-flex gap-2">
-                <div className="text-warning">{p.rating}<i className="fas fa-star"></i></div>
-                <span>{p.reviews.length}{t(" review")}</span>
-              </div>
-              <div className="offer-price d-flex gap-2">
-                <div className="before-offer text-decoration-line-through">${(p.price/(1-(p.discountPercentage)/100)).toFixed(2)}</div>
-                <div className="after-offer text-danger fs-5">${p.price}</div>
-              </div>
-              <div className="bar">
-                <div style={{ "width": p.stock + "%" }}></div>
-              </div>
-              <p className="pt-3 text-center">{t("the available products:")} <span className="fw-bold fs-3 text-primary">{p.stock}</span></p>
-            </div>
-          </div>
           ))}
         </div>
-
       </div>
       <div className="row row-cols-12 my-4 d-flex gap-5">
-        <div className="col-12 col-lg rounded p-5 viewer-to-left d-flex flex-column align-items-end bg-img-red-public" style={{backgroundSize:"cover" ,backgroundPosition:"0 75%"}}>
+        <div
+          className="col-12 col-lg rounded p-5 viewer-to-left d-flex flex-column align-items-end bg-img-red-public"
+          style={{ backgroundSize: "cover", backgroundPosition: "0 75%" }}
+        >
           <p className="viewer-blur fs-3">{t("High-quality products")}</p>
-          <p className="viewer-blur" style={{color:"var(--text-color)"}}>{t("Meat, fish, eggs and all foods")}</p>
-          <Link to="/shop" className="btn btn-info px-3 rounded-5 viewer-blur">{t("shop now")}</Link>
+          <p className="viewer-blur" style={{ color: "var(--text-color)" }}>
+            {t("Meat, fish, eggs and all foods")}
+          </p>
+          <Link to="/shop" className="btn btn-info px-3 rounded-5 viewer-blur">
+            {t("shop now")}
+          </Link>
         </div>
-        <div className="col-12 col-lg rounded p-5 viewer-to-left bg-img-blue-public" style={{backgroundSize:"cover" ,backgroundPosition:"0 75%"}}>
+        <div
+          className="col-12 col-lg rounded p-5 viewer-to-left bg-img-blue-public"
+          style={{ backgroundSize: "cover", backgroundPosition: "0 75%" }}
+        >
           <p className="viewer-blur fs-3">{t("High-quality products")}</p>
-          <p className="viewer-blur" style={{color:"var(--text-color)"}}>{t("Pharaonic style jewelry")}</p>
-          <Link to="/shop" className="btn btn-danger px-3 rounded-5 viewer-blur">{t("shop now")}</Link>
+          <p className="viewer-blur" style={{ color: "var(--text-color)" }}>
+            {t("Pharaonic style jewelry")}
+          </p>
+          <Link
+            to="/shop"
+            className="btn btn-danger px-3 rounded-5 viewer-blur"
+          >
+            {t("shop now")}
+          </Link>
         </div>
       </div>
     </div>
-    )
+  );
 }
 
-export default Hero
-
+export default Hero;
